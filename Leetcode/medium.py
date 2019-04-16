90770cf8188f40a7a786badc1e0a0b044bfe28ad
Silence <daniel.s.mo503@gmail.com>
2019-04-17 01:07:48 +0800

    add #33

--- Leetcode/medium.py
+++ Leetcode/medium.py
@@ -296,6 +296,26 @@ def nextPermutation(nums: list) -> None:
     nums.sort()
     return
 
+def search(nums: list, target: int) -> int:        
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = round((left + right) / 2)     
+        if nums[mid] == target:
+            return mid     
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target <= nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] <= target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    
+    return -1
+
 def findKthLargest(nums, k):
     nums.sort(reverse=True)
     return nums[k - 1]
