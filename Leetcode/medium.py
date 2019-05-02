ff222204808d10acf7720535c2e31cb7d7cdf754
Silence <daniel.s.mo503@gmail.com>
2019-05-02 13:43:53 +0800

    add #56

--- Leetcode/medium.py
+++ Leetcode/medium.py
@@ -403,6 +403,20 @@ def spiralOrder(matrix: list) -> list:
 
     return ret
 
+def merge(intervals: list) -> list:
+    if not intervals:
+        return []
+    
+    intervals.sort(key=lambda x:x[0])
+    ret = [intervals[0]]
+    for i in range(1, len(intervals)):
+        if intervals[i][0] <= ret[-1][1]:
+            ret[-1][1] = max(ret[-1][1], intervals[i][1])
+        else:
+            ret.append(intervals[i])
+    
+    return ret
+
 def findKthLargest(nums, k):
     nums.sort(reverse=True)
     return nums[k - 1]
