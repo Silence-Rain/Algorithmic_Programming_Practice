34193d6ae1d7563bd1bbb024f0a8d8b62a30fb19
Silence <daniel.s.mo503@gmail.com>
2019-05-04 02:55:45 +0800

    add #57

--- Leetcode/hard.py
+++ Leetcode/hard.py
@@ -124,3 +124,19 @@ def firstMissingPositive(nums: list) -> int:
             if flags[i] == False:
                 return i + 1
 
+def insert(intervals: list, newInterval: list) -> list:
+    left, right = [], []
+    start, end = newInterval[0], newInterval[1]
+    
+    for i in intervals:
+        if i[1] < start:
+            left.append(i)
+        elif i[0] > end:
+            right.append(i)
+        else:
+            start = min(i[0], start)
+            end = max(i[1], end)
+    
+    return left + [[start, end]] + right
+
+
