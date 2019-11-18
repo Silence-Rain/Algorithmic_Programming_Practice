12f79e464f58849b5923179efd31b2cf99f516ef
Silence-Rain <daniel.s.mo503@gmail.com>
2019-11-17 23:08:34 -0500

    add #146

--- Leetcode/linked_list/medium.py
+++ Leetcode/linked_list/medium.py
@@ -162,6 +162,37 @@ def sortedListToBST(head: ListNode) -> TreeNode:
     
     return root
 
+# 146. LRU Cache
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.priority = []
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.priority.remove(key)
+            self.priority.append(key)
+            return self.cache[key]
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            if len(self.priority) < self.capacity:
+                self.priority.append(key)
+                self.cache[key] = value
+            else:
+                ind = self.priority.pop(0)
+                del self.cache[ind]
+                self.priority.append(key)
+                self.cache[key] = value
+        else:
+            self.priority.remove(key)
+            self.priority.append(key)
+            self.cache[key] = value
+
 # 147. Insertion Sort List
 def insertionSortList(head: ListNode) -> ListNode:
     dummy, p = ListNode(0), head
